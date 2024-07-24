f52abed36ba8beb28eb0ca5e7f3fd62bd4d76c82
Magolizen34 <luismagolo3@gmail.com>
2024-07-24 12:19:12 +0100

    Add latex to all models

--- utils/kmeans.py
+++
@@ -1,9 +0,0 @@
-from sklearn.cluster import KMeans
-
-class KMeansClustering:
-    def __init__(self, n_clusters):
-        self.n_clusters = n_clusters
-        self.model = KMeans(n_clusters=self.n_clusters)
-
-    def fit_predict(self, data):
-        return self.model.fit_predict(data)
